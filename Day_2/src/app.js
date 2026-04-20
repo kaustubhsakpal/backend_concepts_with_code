0956cd594e523be6ea90f0aa7a085744627870f7
kaustubhsakpal <kaustubhsakpal9@gmail.com>
2026-04-20 22:39:09 +0530

    Update app.js

--- Day_2/src/app.js
+++ Day_2/src/app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const app =express();
 
 function connecttodb(){
-    mongoose.connect('mongodb+srv://kaustubhsakpal9_db_user:WImYjy70P8eXbgvF@cluster0.g5d3mmg.mongodb.net/day-2')
+    mongoose.connect('**********************************************************************************************/day-2')
     .then(()=>{
         console.log("connection done ");
         
